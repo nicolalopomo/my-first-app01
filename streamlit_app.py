31b96c7fde098e1d1ca3682527eeee17367f88dc
nicolalopomo <nicola.lopomo@polimi.it>
2026-04-02 10:47:05 +0000

    Other changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app on Streamlit")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("MY WEB APPLICATION!")
+
+st.text("Yeh, this is really my first app!",
+        text_alignment="right")
