6b0a658cdda9ce90edf64fb5dcde99120e395fd3
nicolalopomo <nicola.lopomo@polimi.it>
2026-04-02 10:53:59 +0000

    I added the slider

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ st.title("MY WEB APPLICATION!")
 
 st.text("Yeh, this is really my first app!",
         text_alignment="right")
+
+
+data = st.slider("Select a specific value", 0, 30, 15)
+
+st.write("The selected value is:", data)
