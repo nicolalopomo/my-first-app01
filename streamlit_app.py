f382691e3f2b7496658a2c98c7f8eb6c5d3795a6
nicolalopomo <nicola.lopomo@polimi.it>
2026-04-22 20:17:02 +0000

    sis asaxs

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,21 +2,18 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+import streamlit as st
 
-with st.sidebar:
-    st.header("Sidebar")
-    st.write("This is the sidebar of the app.")
-    st.radio("Select a page", ["Page 1", "Page 2", "Page 3"], key="page_selection"  )
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
 
-st.write("This is the main page of the app.")
-
-if st.session_state.page_selection == "Page 1":
-    st.write("You are on Page 1.")
-elif st.session_state.page_selection == "Page 2":
-    st.write("You are on Page 2.")
-elif st.session_state.page_selection == "Page 3":
-    col1, col2 = st.columns(2)
-    with col1:
-        st.image("https://upload.wikimedia.org/wikipedia/commons/1/15/Cat_August_2010-4.jpg")
-    with col2:
-        st.image("https://upload.wikimedia.org/wikipedia/commons/0/06/Blue_merle_koolie_short_coat_heading_sheep_%28cropped%29.jpg")
+st.write
