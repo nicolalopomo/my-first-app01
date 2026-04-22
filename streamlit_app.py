49bb095e16d87fc9a5120bd2a37e4e8ed743f20e
nicolalopomo <nicola.lopomo@polimi.it>
2026-04-22 21:40:23 +0000

    jasjda

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,20 +2,18 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 
-st.set_page_config(
-    page_title="Ex-stream-ly Cool App",
-    page_icon="🧊",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
-    }
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read(
+    worksheet="Sheet1",
+    ttl="10m",
+    usecols=[0, 1],
+    nrows=1,
 )
 
-st.title("Ciaoo!")
-
-st.button("Cliccami")
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
