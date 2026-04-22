b8fa509769a94c6dc80bf164e9f2722e58d94801
nicolalopomo <nicola.lopomo@polimi.it>
2026-04-22 22:14:40 +0000

    done

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,10 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-
 from streamlit_gsheets import GSheetsConnection
 
-# Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read(
-    worksheet="Sheet1",
-    ttl="10m",
-    usecols=[0, 1],
-    nrows=1,
-)
+public_url = st.secrets.connections.gsheets["spreadsheet"]
+
+df = conn.read(spreadsheet=st.secrets.connections.gsheets["spreadsheet"])
 
 st.dataframe(df)
